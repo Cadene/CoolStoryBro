be4b70a01c3d2a212021b24b19f0b1439da7b983
Tamazy <tamazy@laposte.net>
2014-10-10 15:40:50 +0200

    first commit

---
+++ workspace/Test/src/Client.java
@@ -0,0 +1,157 @@
+/**
+ * 
+ */
+
+import java.util.Set;
+
+/** 
+ * <!-- begin-UML-doc -->
+ * <!-- end-UML-doc -->
+ * @author 3000693
+ * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class Client extends Personne {
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object attribut1;
+
+	/** 
+	 * @return attribut1
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getAttribut1() {
+		// begin-user-code
+		return attribut1;
+		// end-user-code
+	}
+
+	/** 
+	 * @param attribut1 attribut1 à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setAttribut1(Object attribut1) {
+		// begin-user-code
+		this.attribut1 = attribut1;
+		// end-user-code
+	}
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object attribut2;
+
+	/** 
+	 * @return attribut2
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getAttribut2() {
+		// begin-user-code
+		return attribut2;
+		// end-user-code
+	}
+
+	/** 
+	 * @param attribut2 attribut2 à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setAttribut2(Object attribut2) {
+		// begin-user-code
+		this.attribut2 = attribut2;
+		// end-user-code
+	}
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object attribut3;
+
+	/** 
+	 * @return attribut3
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getAttribut3() {
+		// begin-user-code
+		return attribut3;
+		// end-user-code
+	}
+
+	/** 
+	 * @param attribut3 attribut3 à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setAttribut3(Object attribut3) {
+		// begin-user-code
+		this.attribut3 = attribut3;
+		// end-user-code
+	}
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Set<Banque> banque;
+
+	/** 
+	 * @return banque
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Set<Banque> getBanque() {
+		// begin-user-code
+		return banque;
+		// end-user-code
+	}
+
+	/** 
+	 * @param banque banque à définir
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setBanque(Set<Banque> banque) {
+		// begin-user-code
+		this.banque = banque;
+		// end-user-code
+	}
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void Operation1() {
+		// begin-user-code
+		// TODO Module de remplacement de méthode auto-généré
+
+		// end-user-code
+	}
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void Operation2() {
+		// begin-user-code
+		// TODO Module de remplacement de méthode auto-généré
+
+		// end-user-code
+	}
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void Operation3() {
+		// begin-user-code
+		// TODO Module de remplacement de méthode auto-généré
+
+		// end-user-code
+	}
+}
